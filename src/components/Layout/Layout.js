e0af7bf14578014739cf82db7e3fd5ef047eb60b
Matteo <matforc@hotmail.it>
2018-02-07 22:59:01 +0100

    Starting project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => {
+
+    return (
+
+        <Aux>
+        <div>Toolbar,Sidedrawer, Bacldrop</div>
+    
+        <main>
+            {props.children}
+        </main>
+    
+        </Aux>
+    
+    );
+}
+
+ 
+export default layout;
