d7aa5ee54fa7c7a40f1fc712cfb63b8ba0449ac1
matforc <matforc@hotmail.it>
2018-02-09 12:24:42 +0100

    calculating ingredients number

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,20 +4,43 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
-const burger = (props) => {
-    
-    //Siccome da props ci arriverebbe un oggetto e non un array, dobbiamo convertire ingredient obj 
-    //in array cosi da estrapolare un key name per ogni inglredient e risalire anche al valore di
-    //questo key name, che non è altro che la quantità, qui di seguito viene mostrato come
-    
-    const transformedIngredients = Object.keys(props.ingredients)
-        .map(ingredientKey => {
-                return [...Array(props.ingredients[ingredientKey])].map((elm, i) => {
-                    return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
-                });
-        });
+const burger = (props /* from containers/BurgerBuilder/BurgerBuilder.js */ ) => {
 
+    let transformedIngredients = Object.keys(props.ingredients)
+        /* il metodo Object.keys() qui sopra mi ritorna un Array 
+         di ingredienti es. ['salas','cheese','meat'] ecc.., 
+        come puoi notare è un array di stringhe con il classico index value (0,1,2 ecc..)
+        Sostanzialmente il metodo qui sopra mi serve per prendermi i nomi degli ingredienti 
+        che nell'oggetto PROPS.INGREDIENTS erano sotto forma di CHIAVE (di tipo stringa ovviamente
+        essendo un Object).
+        */
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])]
+             /* Il metodo map(ingKey => qui sopra crea un array per ogni elemento dell'array 
+            soprastante (Object.keys ovvero ['salas','cheese','meat'] ecc..), 
+            queste array contengono elementi undefined nella stessa quantità 
+            del valore dell'oggetto props.ingredients  */   
+            .map((_, i) => {
+                             return <BurgerIngredient key={ingKey + i} type={ingKey} />
+                        }); 
 
+            /* il metodo qui sopra ritorna un componente BurgerIngredient per ogni elemento 
+             di ogni array con elementi undefined creata con il metodo map(ingKey => qui sopra, in questo compenente 
+             gli si aggiunge un attributo key unico e il tipo */
+
+        })
+         /* Il metodo map(ingKey => qui sopra crea un array per ogni elemento dell'array 
+            soprastante (Object.keys ovvero ['salas','cheese','meat'] ecc..), 
+            queste array contengono elementi undefined nella stessa quantità 
+            del valore dell'oggetto props.ingredients  */   
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+        if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please add some ingredient</p>;
+        }
+        
 
 
     return (
