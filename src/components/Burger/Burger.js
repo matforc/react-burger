c5eed85403ed10fecb3b2b09f345480ad640ffff
matforc <matforc@hotmail.it>
2018-02-09 08:49:48 +0100

    preparing dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    
+    //Siccome da props ci arriverebbe un oggetto e non un array, dobbiamo convertire ingredient obj 
+    //in array cosi da estrapolare un key name per ogni inglredient e risalire anche al valore di
+    //questo key name, che non è altro che la quantità, qui di seguito viene mostrato come
+    
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+                return [...Array(props.ingredients[ingredientKey])].map((elm, i) => {
+                    return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+                });
+        });
+
+
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+           {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,25 @@ import Aux from '../../hoc/Aux'
 
 class BurgerBuilder extends Component {
     
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+    
     render() { 
         return (  
             <Aux>
-                <Burger></Burger>
+                {/*questo sarebbe la props da passare a burger component ingredients={this.state.ingredients}*/}
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Control</div>
             </Aux> 
             
