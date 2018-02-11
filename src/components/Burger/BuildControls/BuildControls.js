60bfdf4f10170fb9a62044481e4bc22c4b78e3a8
Matteo <matforc@hotmail.it>
2018-02-11 19:17:13 +0100

    added total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -20,12 +20,14 @@ const controls = [
 const buildControls = (props /* from containers/BurgerBuilder/BurgerBuilder.js */) => {
     return (
         <div className={classes.BuildControls}>
+        <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => {
            return <BuildControl 
            key={ctrl.label} 
            label={ctrl.label} 
            added={ () => props.ingredientAdded(ctrl.type) }
-           removed={ () => props.ingredientRemoved(ctrl.type) }/>
+           removed={ () => props.ingredientRemoved(ctrl.type) }
+           disabled={props.disabled[ctrl.type]}/>
         })}
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -49,6 +49,10 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0 ){
+            return;
+        }
+
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
@@ -66,6 +70,12 @@ class BurgerBuilder extends Component {
     }
     
     render() { 
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0 
+        }
         
         return (  
             <Aux>
@@ -74,6 +84,8 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}
+                price={this.state.totalPrice}
                 />
             </Aux> 
             
